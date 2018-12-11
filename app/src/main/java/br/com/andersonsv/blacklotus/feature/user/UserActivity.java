344de981af3fcc07c43d8dbb496aad4776a89bcb
ANDERSON S VIEIRA <anderson.vieira-ext@viavarejo.com.br>
2018-12-11 13:32:16 -0200

    Implementacao de cadastro de login

--- app/src/main/java/br/com/andersonsv/blacklotus/feature/user/UserActivity.java
+++ app/src/main/java/br/com/andersonsv/blacklotus/feature/user/UserActivity.java
@@ -1,15 +1,41 @@
 package br.com.andersonsv.blacklotus.feature.user;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.firebase.ui.auth.data.model.User;
+import com.firebase.ui.auth.util.ui.fieldvalidators.BaseValidator;
+import com.firebase.ui.auth.util.ui.fieldvalidators.EmailFieldValidator;
+import com.firebase.ui.auth.util.ui.fieldvalidators.PasswordFieldValidator;
+import com.firebase.ui.auth.viewmodel.email.EmailProviderResponseHandler;
+import com.google.firebase.auth.FirebaseAuth;
+
 import br.com.andersonsv.blacklotus.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class UserActivity extends AppCompatActivity {
+
+    private EmailProviderResponseHandler mHandler;
+    private EmailFieldValidator mEmailFieldValidator;
+    private PasswordFieldValidator mPasswordFieldValidator;
+    private BaseValidator mNameValidator;
+
+    private User mUser;
+    private FirebaseAuth auth;
+
+    @BindView(R.id.textInputEditTextEmail)
+    TextInputEditText mEmail;
+
+    @BindView(R.id.textInputEditTextPassword)
+    TextInputEditText mPassword;
+
+    @BindView(R.id.textInputEditTextPasswordConfirmation)
+    TextInputEditText mPasswordConfirmation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +44,19 @@ public class UserActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.buttonSignUp)
+    public void signUp(View view) {
+
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+        String name = mPasswordConfirmation.getText().toString();
+
+       // boolean emailValid = mEmailFieldValidator.validate(email);
+       // boolean passwordValid = mPasswordFieldValidator.validate(password);
+       // boolean nameValid = mNameValidator.validate(name);
+
+    }
 }
