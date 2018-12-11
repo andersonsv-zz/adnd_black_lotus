683ed0ab593dd8045722c4d257704cdbe675e918
ANDERSON S VIEIRA <anderson.vieira-ext@viavarejo.com.br>
2018-12-11 17:26:08 -0200

    Ajustes de layout e inicio da implementacao de usuario

--- app/src/main/java/br/com/andersonsv/blacklotus/feature/user/UserActivity.java
+++ app/src/main/java/br/com/andersonsv/blacklotus/feature/user/UserActivity.java
@@ -1,17 +1,33 @@
 package br.com.andersonsv.blacklotus.feature.user;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.Resource;
 import com.firebase.ui.auth.data.model.User;
+import com.firebase.ui.auth.util.GoogleApiUtils;
 import com.firebase.ui.auth.util.ui.fieldvalidators.BaseValidator;
 import com.firebase.ui.auth.util.ui.fieldvalidators.EmailFieldValidator;
 import com.firebase.ui.auth.util.ui.fieldvalidators.PasswordFieldValidator;
 import com.firebase.ui.auth.viewmodel.email.EmailProviderResponseHandler;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+
+import java.util.Arrays;
+import java.util.List;
 
 import br.com.andersonsv.blacklotus.R;
 import butterknife.BindView;
@@ -20,13 +36,9 @@ import butterknife.OnClick;
 
 public class UserActivity extends AppCompatActivity {
 
-    private EmailProviderResponseHandler mHandler;
-    private EmailFieldValidator mEmailFieldValidator;
-    private PasswordFieldValidator mPasswordFieldValidator;
-    private BaseValidator mNameValidator;
-
     private User mUser;
-    private FirebaseAuth auth;
+    private FirebaseAuth mFirebaseAuth;
+    private List<AuthUI.IdpConfig> providers;
 
     @BindView(R.id.textInputEditTextEmail)
     TextInputEditText mEmail;
@@ -37,14 +49,19 @@ public class UserActivity extends AppCompatActivity {
     @BindView(R.id.textInputEditTextPasswordConfirmation)
     TextInputEditText mPasswordConfirmation;
 
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
     }
 
     @OnClick(R.id.buttonSignUp)
@@ -54,9 +71,14 @@ public class UserActivity extends AppCompatActivity {
         String password = mPassword.getText().toString();
         String name = mPasswordConfirmation.getText().toString();
 
-       // boolean emailValid = mEmailFieldValidator.validate(email);
-       // boolean passwordValid = mPasswordFieldValidator.validate(password);
-       // boolean nameValid = mNameValidator.validate(name);
+        mFirebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+            }
+        });
+
+        //check email Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
     }
 }
