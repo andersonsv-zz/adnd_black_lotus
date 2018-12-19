7f92b3739f7af03d511402d1fed0efff77156c83
ANDERSON S VIEIRA <anderson.vieira-ext@viavarejo.com.br>
2018-12-19 18:19:21 -0200

    Alteração para fazer as chamadas de cores

--- app/src/main/java/br/com/andersonsv/blacklotus/feature/main/AddDeckFragment.java
+++ app/src/main/java/br/com/andersonsv/blacklotus/feature/main/AddDeckFragment.java
@@ -1,18 +1,33 @@
 package br.com.andersonsv.blacklotus.feature.main;
 
 import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
 import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.v4.app.Fragment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.common.primitives.Ints;
+import com.thebluealliance.spectrum.SpectrumDialog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.andersonsv.blacklotus.R;
 import br.com.andersonsv.blacklotus.firebase.Deck;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AddDeckFragment extends Fragment {
 
@@ -31,39 +46,115 @@ public class AddDeckFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_add_deck, container, false);
 
         ButterKnife.bind(this, rootView);
+        deck = new Deck();
+
+        updateColor();
 
         return rootView;
     }
 
-    private void updateColor(){
-
-        if (deck.getColor1() == null) {
-
-        }
-
-        /*GradientDrawable gradientDrawable = new GradientDrawable(
-                GradientDrawable.Orientation.TOP_BOTTOM,
-                new int[]{Color.WHITE,
-                        Color.BLUE,
-                });
-
-        gradientDrawable.setCornerRadii(new float[] { 50, 50, 50, 50, 50, 50, 50, 50 });
-        holder.getmColor().setImageDrawable(gradientDrawable);*/
+    @OnClick(R.id.buttonAddColor)
+    public void addColor(View view){
+        new SpectrumDialog.Builder(view.getContext())
+            .setColors(R.array.deck_colors)
+            .setTitle(R.string.deck_add_color_title)
+            .setDismissOnColorSelected(false)
+            .setPositiveButtonText(R.string.deck_select_color_confirm)
+            .setOnColorSelectedListener(new SpectrumDialog.OnColorSelectedListener() {
+                @Override public void onColorSelected(boolean positiveResult, @ColorInt int color) {
+                    if (positiveResult) {
+                        checkNextColor(color);
+                    }
+                }
+            }).build().show(getFragmentManager(), "dialog_demo_1");
     }
 
+    private void checkNextColor(int color){
 
-    /*new SpectrumDialog.Builder(getContext())
-            .setColors(R.array.demo_colors)
-                .setSelectedColorRes(R.color.md_blue_500)
-                .setDismissOnColorSelected(true)
-                .setOutlineWidth(2)
-                .setOnColorSelectedListener(new SpectrumDialog.OnColorSelectedListener() {
-        @Override public void onColorSelected(boolean positiveResult, @ColorInt int color) {
-            if (positiveResult) {
-                Toast.makeText(getContext(), "Color selected: #" + Integer.toHexString(color).toUpperCase(), Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getContext(), "Dialog cancelled", Toast.LENGTH_SHORT).show();
-            }
+        String colorHex = "#".concat(Integer.toHexString(color).toUpperCase());
+
+        boolean existsColor = checkColorExists(colorHex);
+
+        if (existsColor) {
+            Toast.makeText(getContext(), R.string.deck_color_exists_error, Toast.LENGTH_LONG);
+            return;
         }
-    }).build().show(getFragmentManager(), "dialog_demo_1");*/
+
+        if (deck.getColor1() == null) {
+            deck.setColor1(colorHex);
+        } else if (deck.getColor2() == null) {
+            deck.setColor2(colorHex);
+        } else if (deck.getColor3() == null) {
+            deck.setColor3(colorHex);
+        } else if (deck.getColor4() == null) {
+            deck.setColor4(colorHex);
+        } else {
+            deck.setColor5(colorHex);
+        }
+        updateColor();
+    }
+
+    private boolean checkColorExists(String color) {
+        if (deck.getColor1() != null && deck.getColor1().equals(color)) {
+            return true;
+        } else if (deck.getColor2() != null && deck.getColor2().equals(color)) {
+            return true;
+        } else if (deck.getColor3() != null && deck.getColor3().equals(color)) {
+            return true;
+        } else if (deck.getColor4() != null && deck.getColor4().equals(color)) {
+            return true;
+        } else if (deck.getColor5() != null && deck.getColor5().equals(color)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void updateColor(){
+
+        List<Integer> colors = new ArrayList<>();
+
+        if (deck.getColor1() == null) {
+            setOneColor(Color.GRAY);
+            return;
+        } else if (deck.getColor1() != null && deck.getColor2() == null) {
+
+            int colorValue = Color.parseColor(deck.getColor1());
+            setOneColor(colorValue);
+            return;
+
+        } else {
+            addColorIfNonNull(deck.getColor1(), colors);
+            addColorIfNonNull(deck.getColor2(), colors);
+            addColorIfNonNull(deck.getColor3(), colors);
+            addColorIfNonNull(deck.getColor4(), colors);
+            addColorIfNonNull(deck.getColor5(), colors);
+        }
+
+        int[] colorArray = Ints.toArray(colors);
+
+        if (colorArray.length > 1){
+            GradientDrawable gradientDrawable = new GradientDrawable(
+                    GradientDrawable.Orientation.TOP_BOTTOM,
+                    colorArray);
+            gradientDrawable.setShape(GradientDrawable.OVAL);
+            mColor.setImageDrawable(gradientDrawable);
+        } else {
+            setOneColor(Color.GRAY);
+        }
+    }
+
+    private void setOneColor(int color){
+        ShapeDrawable sd = new ShapeDrawable(new OvalShape());
+        sd.setIntrinsicHeight(100);
+        sd.setIntrinsicWidth(100);
+        sd.getPaint().setColor(color);
+        mColor.setBackground(sd);
+    }
+
+    private void addColorIfNonNull(final String color, final List<Integer> colors){
+        if (color != null) {
+            int colorValue = Color.parseColor(color);
+            colors.add(colorValue);
+        }
+    }
 }
--- app/src/main/java/br/com/andersonsv/blacklotus/firebase/Deck.java
+++ app/src/main/java/br/com/andersonsv/blacklotus/firebase/Deck.java
@@ -81,4 +81,35 @@ public class Deck {
         return color5;
     }
 
+    public void setNumberOfCards(Integer numberOfCards) {
+        this.numberOfCards = numberOfCards;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setChangeDeck(Boolean changeDeck) {
+        this.changeDeck = changeDeck;
+    }
+
+    public void setColor1(String color1) {
+        this.color1 = color1;
+    }
+
+    public void setColor2(String color2) {
+        this.color2 = color2;
+    }
+
+    public void setColor3(String color3) {
+        this.color3 = color3;
+    }
+
+    public void setColor4(String color4) {
+        this.color4 = color4;
+    }
+
+    public void setColor5(String color5) {
+        this.color5 = color5;
+    }
 }
