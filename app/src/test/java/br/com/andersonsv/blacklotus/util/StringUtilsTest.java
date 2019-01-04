2e81328cd57a5b8bc7d07109bfdef1dea090c797
Your Name <anderson.si.vieira@hotmail.com>
2019-01-03 22:24:02 -0200

    Implementacao de testes

--- app/src/test/java/br/com/andersonsv/blacklotus/util/StringUtilsTest.java
+++ app/src/test/java/br/com/andersonsv/blacklotus/util/StringUtilsTest.java
@@ -1,6 +1,23 @@
 package br.com.andersonsv.blacklotus.util;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class StringUtilsTest {
 
+    @Test
+    public void testNullString() {
+        String replaceType = null;
+        String typeConverted = StringUtils.replaceTypetImgSrc(replaceType);
+        assertEquals(typeConverted, Constants.EMPTY_STRING);
+    }
+
+    @Test
+    public void testReplaceString() {
+        String replaceType = "{W}";
+        String typeConverted = StringUtils.replaceTypetImgSrc(replaceType);
+        assertEquals(typeConverted, Constants.IMAGE_REPLACE.replace("$1", "W"));
+    }
 
 }
