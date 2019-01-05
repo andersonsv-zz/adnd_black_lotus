76153551ef38ea22f20fd74f3cb26edcbc6dad18
Your Name <anderson.si.vieira@hotmail.com>
2019-01-05 10:08:19 -0200

    rarity model unit test

--- app/src/test/java/br/com/andersonsv/blacklotus/model/CardColorTest.java
+++ app/src/test/java/br/com/andersonsv/blacklotus/model/CardColorTest.java
@@ -7,27 +7,27 @@ import static org.junit.Assert.assertEquals;
 public class CardColorTest {
 
     @Test
-    public void getColorCodeById(){
+    public void get_success_color_code_by_id(){
         CardColor cardColor = CardColor.getById("B");
         CardColor expected = CardColor.BLACK;
         assertEquals(cardColor, expected);
     }
 
     @Test
-    public void getColorCodeByCode(){
+    public void get_success_color_code_by_code(){
         CardColor cardColor = CardColor.getByCode("{B}");
         CardColor expected = CardColor.BLACK;
         assertEquals(cardColor, expected);
     }
 
     @Test
-    public void getColorCodeByIdNull(){
+    public void get_null_color_code_by_id(){
         CardColor cardColor = CardColor.getById("Y");
         assertEquals(cardColor, null);
     }
 
     @Test
-    public void getColorCodeByCodeNull(){
+    public void get_null_color_code_by_code(){
         CardColor cardColor = CardColor.getByCode("{Y}");
         assertEquals(cardColor, null);
     }
---
+++ app/src/test/java/br/com/andersonsv/blacklotus/model/RarityTest.java
@@ -0,0 +1,20 @@
+package br.com.andersonsv.blacklotus.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RarityTest {
+    @Test
+    public void get_success_rarity_code_by_type(){
+        Rarity rarity = Rarity.getByType("Common");
+        Rarity expected = Rarity.COMMON;
+        assertEquals(rarity, expected);
+    }
+
+    @Test
+    public void get_null_rarity_code_by_type(){
+        Rarity rarity = Rarity.getByType("Null");
+        assertEquals(rarity, null);
+    }
+}
